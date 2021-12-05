25b0cea2fc507023aa52acd01c660f8a70002ce3
Antoni Spaanderman <56turtle56@gmail.com>
2021-12-05 11:59:51 +0100

    use chat

--- src/main/java/com/antonilol/example_mixins/Main.java
+++ src/main/java/com/antonilol/example_mixins/Main.java
@@ -1,11 +1,22 @@
 package com.antonilol.example_mixins;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.network.MessageType;
+import net.minecraft.text.LiteralText;
 
 public class Main implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
 		System.out.println("hi");
+	}
+	
+	public static void chat(String message) {
+		MinecraftClient c = MinecraftClient.getInstance();
+		if (c == null) {
+			return;
+		}
+		c.inGameHud.addChatMessage(MessageType.SYSTEM, new LiteralText(message), null);
 	}
 }
--- src/main/java/com/antonilol/example_mixins/mixin/ActionBarMixin.java
+++ src/main/java/com/antonilol/example_mixins/mixin/ActionBarMixin.java
@@ -1,5 +1,7 @@
 package com.antonilol.example_mixins.mixin;
 
+import static com.antonilol.example_mixins.Main.chat;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,8 +12,10 @@ import net.minecraft.text.Text;
 
 @Mixin(InGameHud.class)
 public class ActionBarMixin {
+	
 	@Inject(at = @At("HEAD"), method = "Lnet/minecraft/client/gui/hud/InGameHud;setOverlayMessage(Lnet/minecraft/text/Text;Z)V")
 	private void sendMessage(Text message, boolean tinted, CallbackInfo info) {
-		System.out.println("actionbar message: " + message.asString());
+		chat("actionbar message: " + message.asString());
+		chat("tinted: " + tinted);
 	}
 }
