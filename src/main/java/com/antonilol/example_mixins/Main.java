011ceeb25f8e47201388857674826e60634ec702
Antoni Spaanderman <56turtle56@gmail.com>
2021-12-05 11:42:14 +0100

    initial commit

---
+++ src/main/java/com/antonilol/example_mixins/Main.java
@@ -0,0 +1,11 @@
+package com.antonilol.example_mixins;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+		System.out.println("hi");
+	}
+}
